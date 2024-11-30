d4430663f8ce18879465823afbf5e5695e790de8
mustafa yener <dev.mustafa.yener@gmail.com>
2024-11-30 16:49:44 +0300

    [feature] add application-and-dev-layer

---
+++ HotelManagerService/Core/HotelManager.Application/Interfaces/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using HotelManager.Application.Interfaces.Repositories;
+using HotelManager.Domain.Common;
+
+namespace HotelManager.Application.Interfaces.UnitOfWorks
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        IReadRepository<T> GetReadRepostory<T>() where T : class, IEntityBase, new();
+        IWriteRepository<T> GetWriteRepostory<T>() where T : class, IEntityBase, new();
+        Task<int> SaveAsync();
+        int Save();
+    }
+}
