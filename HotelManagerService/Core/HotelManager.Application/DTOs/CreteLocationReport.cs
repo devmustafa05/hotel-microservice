9c85637312358c96f8bdf3713e7f33ad88c4474b
mustafa yener <dev.mustafa.yener@gmail.com>
2024-12-03 15:09:36 +0300

    [feature] added dto

--- HotelManagerService/Core/HotelManager.Application/DTOs/CreteLocationReport.cs
+++ HotelManagerService/Core/HotelManager.Application/DTOs/CreteLocationReport.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HotelManager.Application.DTOs
+﻿namespace HotelManager.Application.DTOs
 {
     public class CreteLocationReport
     {
-        public int Id { get; set; }
+        public int LocationId { get; set; }
+        public required string ReportDocumentId { get; set; }
         public decimal Latitude { get; set; }
         public decimal Longitude { get; set; }
     }
---
+++ HotelReportService/Src/ReportService.Application/DTOs/ResultLocationReport/HotelDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportService.Application.DTOs.ResultLocationReport
+{
+    public class HotelDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? LocationName { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public List<HotelOfficialDto> HotelOfficials { get; set; }
+        public List<HotelContactsDto> HotelContacts { get; set; }
+        public List<HotelLocationContactDto> HotelLocationContacts { get; set; }
+    }
+}
